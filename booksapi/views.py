07de94468ad06b73e10bd4f3ef02adc6ad53ecb6
Betwist <Sh0.ker481@gmail.com>
2023-11-24 13:47:50 +0300

    Create book model and add CRUD

--- booksapi/views.py
+++ booksapi/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+class BookListCreateView(generics.ListCreateAPIView):
+    """Получение информации о всех книгах и добавление новой"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """Получение, обновление и удаление конкретной книги"""
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
